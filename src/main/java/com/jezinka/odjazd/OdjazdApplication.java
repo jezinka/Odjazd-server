d18cde80da6e3a97c58c27930d1249641b5e41ec
jezinka <jezina1989@gmail.com>
2018-04-08 12:03:30 +0200

    initial commit of Odjazd-reboot

---
+++ src/main/java/com/jezinka/odjazd/OdjazdApplication.java
@@ -0,0 +1,12 @@
+package com.jezinka.odjazd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdjazdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdjazdApplication.class, args);
+    }
+}
---
+++ src/main/java/com/jezinka/odjazd/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.jezinka.odjazd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Witaj świecie!";
+    }
+}
