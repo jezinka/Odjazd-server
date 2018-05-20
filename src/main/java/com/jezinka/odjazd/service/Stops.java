3a63a2336a259267f995cf9c8a0cc4fc20e6de75
jezinka <jezina1989@gmail.com>
2018-05-20 12:55:36 +0200

    delete unused class

--- src/main/java/com/jezinka/odjazd/service/Stops.java
+++
@@ -1,8 +0,0 @@
-package com.jezinka.odjazd.service;
-
-public class Stops {
-    public static final int OSIEDLE_SOBIESKIEGO_2 = 1946;
-    public static final int OSIEDLE_SOBIESKIEGO_1 = 1945;
-    public static final int PSIE_POLE_RONDO = 3415;
-    public static final int MULICKA = 3195;
-}
